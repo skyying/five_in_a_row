55a9b247047ae2dfe811cbf393c2940215b14682
skyying <dreamuk@gmail.com>
2020-03-01 20:08:48 +0800

    Updates

---
+++ precache-manifest.1b98045da24925a8e1cf50c82e6cca0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b2bd084fc665c041c15cc65cff3126",
+    "url": "/five_in_a_row/index.html"
+  },
+  {
+    "revision": "4e225b2b30e568fda575",
+    "url": "/five_in_a_row/static/css/main.79345e0c.chunk.css"
+  },
+  {
+    "revision": "32fce8f2cd69d6617b07",
+    "url": "/five_in_a_row/static/js/2.8db6cb68.chunk.js"
+  },
+  {
+    "revision": "4e225b2b30e568fda575",
+    "url": "/five_in_a_row/static/js/main.d3bcb8f2.chunk.js"
+  },
+  {
+    "revision": "263a43406dace6215e3a",
+    "url": "/five_in_a_row/static/js/runtime~main.27bc2ab5.js"
+  }
+]);
